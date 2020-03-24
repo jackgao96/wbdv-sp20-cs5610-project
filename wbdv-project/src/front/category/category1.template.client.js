59a0aaf8910a610b2a20bb98aefa8dabdb4bfb7a
Vinay <vinay1020287@gmail.com>
2020-03-23 20:27:29 -0400

    Added category components

---
+++ wbdv-project/src/front/category/category1.template.client.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './CategoryTableComponent'
+import CategoryTableComponent from "./CategoryTableComponent";
+
+class Category1Component extends React.Component {
+    state = {
+        stocks: [],
+        watchList: []
+    }
+
+    componentDidMount = async () => {
+        //TODO: function to fetch the category's stocks and user's watchlist
+        const stocks = await getCategoryStocks("Category 1")
+        const watchList = fetchWatchlist(user)
+        this.setState({
+            stocks: stocks,
+            watchList: watchList
+        })
+    }
+
+    addToWatchList = async (stock) => {
+        this.setState(prevState => ({
+            watchList: [...prevState.watchList, stock]
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <CategoryTableComponent
+                    category="Category 1"
+                    stocks={stocks}
+                    addToWatchList={this.addToWatchList}
+                />
+            </div>
+        )
+    }
+}
+
+
+export default Category1Component
