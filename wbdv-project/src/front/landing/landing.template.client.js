4190f025f1c08ef721b7382a2321c43c849c18ef
Huskyjjy <jin.jingy@husky.neu.edu>
2020-04-01 15:53:36 -0400

    vedio

--- wbdv-project/src/front/landing/landing.template.client.js
+++ wbdv-project/src/front/landing/landing.template.client.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./LandingPageContainer.css"
+import id from './dollar.mp4'
 
 class LandingPageContainer extends React.Component{
     render() {
         return(
-            <div id = "background-container">
-
-                <div id="header" className="container-fluid row">
+            <div>
+            <div class="overlay"></div>
+            <video playsinline autoPlay muted loop id="bgVideo">
+                <source src={id} type="video/mp4"/>
+            </video>
+            <div id = "container h-100">
+                <div id="header" className="d-flex row">
                     <h1 className="col-3">Stocks4all</h1>
                     <h4 className="col-3 offset-6">About</h4>
                 </div>
-                <div id="access" className="container">
+                <div id="access" className="container fluid">
                     <button className="btn btn-primary">Continue as a guest</button>
                     <button className="btn btn-primary">Login now!</button>
                     <button className="btn btn-primary">Register as a new user</button>
                 </div>
-                <video autoPlay muted loop id="bgVideo">
-                    <source src="dollar.mp4" type="video/mp4"/>
-                </video>
+            </div>
             </div>
 
         );
